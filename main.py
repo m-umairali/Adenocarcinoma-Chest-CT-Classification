ce1b031a6b6d4f271d532783364bd031dc47b6aa
umair <umairalism@gmail.com>
2024-03-19 06:41:31 +0500

    updated evaluation

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from chestclassifier import logger
 from chestclassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from chestclassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 from chestclassifier.pipeline.stage_03_model_trainer import ModelTrainingPipeline
+from chestclassifier.pipeline.stage_04_model_evaluation import EvaluationPipeline
 
 
 
@@ -38,6 +39,19 @@ try:
    model_trainer = ModelTrainingPipeline()
    model_trainer.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+    
+    
+STAGE_NAME = "Evaluation stage"
+try:
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   model_evalution = EvaluationPipeline()
+   model_evalution.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+
 except Exception as e:
         logger.exception(e)
         raise e
--- src/chestclassifier/config/configuration.py
+++ src/chestclassifier/config/configuration.py
@@ -3,7 +3,8 @@ from chestclassifier.constants import *
 from chestclassifier.utils.common import read_yaml, create_directories
 from chestclassifier.entity.config_entity import (DataIngestionConfig,
                                                   PrepareBaseModelConfig,
-                                                  TrainingConfig)
+                                                  TrainingConfig,
+                                                  EvaluationConfig)
 
 
 class ConfigurationManager:
@@ -71,3 +72,18 @@ class ConfigurationManager:
         )
 
         return training_config
+    
+    
+    def get_evaluation_config(self) -> EvaluationConfig:
+        eval_config = EvaluationConfig(
+            path_of_model="artifacts/training/model.h5",
+            training_data="artifacts/data_ingestion/Chest-CT-Scan-data",
+            mlflow_uri="https://dagshub.com/m-umairali/Adenocarcinoma-Chest-CT-Classification.mlflow",
+            all_params=self.params,
+            params_image_size=self.params.IMAGE_SIZE,
+            params_batch_size=self.params.BATCH_SIZE
+        )
+        return eval_config
+    
+    
+    
