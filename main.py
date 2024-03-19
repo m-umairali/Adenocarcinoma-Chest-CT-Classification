4451972d62541852ecb7a1afd5931ab9ccadeff7
umair <umairalism@gmail.com>
2024-03-19 05:54:13 +0500

    model training updated

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from chestclassifier import logger
 from chestclassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from chestclassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+from chestclassifier.pipeline.stage_03_model_trainer import ModelTrainingPipeline
 
 
 
@@ -15,3 +16,28 @@ try:
 except Exception as e:
     logger.exception(e)
     raise e
+
+
+STAGE_NAME = "Prepare base model"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   prepare_base_model = PrepareBaseModelTrainingPipeline()
+   prepare_base_model.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+    
+    
+    
+STAGE_NAME = "Training"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   model_trainer = ModelTrainingPipeline()
+   model_trainer.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
